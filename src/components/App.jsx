9a36b572ef9f9bbcd090d91ede53365db51e2bfe
SerhiiMukhin <110244388+SerhiiMukhin@users.noreply.github.com>
2023-03-11 10:10:48 +0200

    at least it works

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,72 +1,74 @@
-import React from 'react';
+// import React from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import css from './App.module.css';
 
-export default class App extends React.Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+const App = () => {
 
-  onSubmit = event => {
+// Видобуваємо контакти з localStorage
+
+  const getLocalContacts = () => {
+    const localContacts = localStorage.getItem('contacts')
+    const parsedContacts = JSON.parse(localContacts)
+    return parsedContacts;
+  }
+
+  // useState для контактів
+
+  const [contacts, setContacts] = useState(getLocalContacts() || []);
+
+  // useState для filter
+
+  const [filter, setFilter] = useState('');
+
+  // useEffect для додавання контактів при зміні state
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  // Обробка Submit
+
+  const onSubmit = event => {
     event.preventDefault();
     const form = event.currentTarget;
     const name = form.elements.name.value;
     const number = form.elements.number.value;
-    if (
-      this.state.contacts.some(({ name: contactName }) => contactName === name)
-    ) {
+    if (contacts.some(({ name: contactName }) => contactName === name)) {
       alert(`${name} is already in contacts!`);
     } else {
-      this.setState(({ contacts }) => ({
-        contacts: [...contacts, { id: nanoid(), name, number }],
-      }));
+      setContacts([...contacts, { id: nanoid(), name, number }]);
       form.reset();
     }
   };
 
-  searchContact = event => {
-    this.setState({ filter: event.target.value });
+  // Перевірка наявності контакту
+  const searchContact = event => {
+    setFilter(event.target.value);
   };
 
-  deleteContact = event => {
+  // Видалення контакту
+  const deleteContact = event => {
     const id = event.currentTarget.value;
-    this.setState({
-      contacts: this.state.contacts.filter(contact => contact.id !== id),
-    });
+    setContacts(contacts.filter(contact => contact.id !== id));
   };
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
+  return (
+    <div className={css.wrapper}>
+      <h1 className={css.main_title}>Phonebook</h1>
+      <ContactForm onSubmit={onSubmit}></ContactForm>
+      <h2 className={css.contacts_title}>Contacts</h2>
+      <Filter filter={filter} searchContact={searchContact}></Filter>
+      <ContactList
+        contacts={contacts}
+        filter={filter}
+        deleteContact={deleteContact}
+      ></ContactList>
+    </div>
+  );
+};
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  render() {
-    const { filter, contacts } = this.state;
-    return (
-      <div className={css.wrapper}>
-        <h1 className={css.main_title}>Phonebook</h1>
-        <ContactForm onSubmit={this.onSubmit}></ContactForm>
-        <h2 className={css.contacts_title}>Contacts</h2>
-        <Filter filter={filter} searchContact={this.searchContact}></Filter>
-        <ContactList
-          contacts={contacts}
-          filter={filter}
-          deleteContact={this.deleteContact}
-        ></ContactList>
-      </div>
-    );
-  }
-}
+export default App;
